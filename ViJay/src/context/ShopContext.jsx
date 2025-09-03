b26e4c371bcd6e214f4996a234b767324e3e4940
ayushigarg24 <ayushigarg24@Ayushi>
2025-09-03 18:07:42 +0530

    files added

---
+++ ViJay/src/context/ShopContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useEffect, useState } from "react";
+import {products} from'../assets/assets'
+import { useNavigate } from "react-router-dom";
+
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+    const currency = '$';
+    const delivery_fee = 10;
+    const [search, setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false);
+    const navigate = useNavigate();
+
+     const value = {
+        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch,navigate
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+
+}
+
+export default ShopContextProvider;
